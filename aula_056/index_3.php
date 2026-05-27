0c044abe9f67a523dc4d612b9d2b4070cd278809
Pegasis12 <plsantana246@gmail.com>
2026-05-26 21:59:02 -0300

    Formas de escrever em um ficheiro

--- aula_056/index_3.php
+++ aula_056/index_3.php
@@ -7,7 +7,7 @@
   $file = fopen("novo.txt", "w");
   for($i = 1; $i <= 1000; $i++){
     fputs($file, "3 x $i = " . (3 * $i) . PHP_EOL);
-    // fwrite() é um alias de fputs
+    // fwrite() é mesma coisa q fputs
   } 
   fclose($file);
 ?>
